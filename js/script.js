91edc1097d5b8e0aa373e5d91a1fe6146ce2c396
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-03-17 08:52:54 +0100

    added configs and a temp user for intellisense

--- js/script.js
+++ js/script.js
@@ -1,34 +1,38 @@
 
+var headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+};
+
+var getConf = { method: 'GET', header: headers };
+var postConf = { method: 'POST', header: headers };
+var putConf = { method: 'PUT', header: headers };
+var deleteConf = { method: 'DELETE', header: headers };
 
 var tbody = document.getElementById("tbody");
 var refresh = document.getElementById("refresh");
 
-var persons = [];
+var persons = [{
+    id: 1,
+    email: "asd@asd.com",
+    firstName: "First Name",
+    lastName: "Last Name"
+}];
 
 function updateTable() {
     tbody.innerHTML = persons.map(function (p) {
-        return "<tr><td>"+
-        p.id +
-        "</td><td>"+
-        p.email +
-        "</td><td>"+
-        p.firstName +
-        "</td><td>"+
-        p.lastName +
-        "</td><td>"+
-        // Buttons?
-        "</td></tr>"
+        return "<tr><td>" +
+            p.id +
+            "</td><td>" +
+            p.email +
+            "</td><td>" +
+            p.firstName +
+            "</td><td>" +
+            p.lastName +
+            "</td><td>" +
+            // Buttons?
+            "</td></tr>"
     }).join("\n");
-}
-
-
-
-
-
-
-
-
-
-
-
-
+};
