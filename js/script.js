654e233ad94c56142d932be7faefa1b35198a035
JamieEgeberg <jamie_egeberg@hotmail.com>
2017-03-17 09:09:39 +0100

    buttons

--- js/script.js
+++ js/script.js
@@ -6,35 +6,33 @@ var headers = {
     'Accept': 'application/json'
 };
 
-var getConf = {method: 'GET', header: headers};
-var postConf = {method: 'POST', header: headers};
-var putConf = {method: 'PUT', header: headers};
-var deleteConf = {method: 'DELETE', header: headers};
+var getConf = { method: 'GET', header: headers };
+var postConf = { method: 'POST', header: headers };
+var putConf = { method: 'PUT', header: headers };
+var deleteConf = { method: 'DELETE', header: headers };
 
 var tbody = document.getElementById("tbody");
 var refresh = document.getElementById("refresh");
 
 var persons = [{
-        id: 1,
-        email: "asd@asd.com",
-        firstName: "First Name",
-        lastName: "Last Name"
-    }];
+    id: 1,
+    email: "asd@asd.com",
+    firstName: "First Name",
+    lastName: "Last Name"
+}];
 
 function updateTable() {
     tbody.innerHTML = persons.map(function (p) {
         return "<tr><td>" +
-                p.id +
-                "</td><td>" +
-                p.email +
-                "</td><td>" +
-                p.firstName +
-                "</td><td>" +
-                p.lastName +
-                "</td><td>" +
-                "<button class= 'btn btn-warning edit' value=" + p.id + ">edit</button> " +
-                "<button class= 'btn btn-danger delete' value=" + p.id + ">delete</button>" +
-                "</td></tr>";
+            p.id +
+            "</td><td>" +
+            p.email +
+            "</td><td>" +
+            p.firstName +
+            "</td><td>" +
+            p.lastName +
+            "</td><td>" +
+            // Buttons?
+            "</td></tr>"
     }).join("\n");
 };
-refresh.addEventListener("click", updateTable);
