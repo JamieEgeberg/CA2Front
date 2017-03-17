9990b68e84ad6f605223690cc5423d95e992f1d9
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-03-17 12:48:52 +0100

    GET api/person, POST api/person, PUT api/person, DELETE api/person

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 var port = 8080;
 var domain = "http://localhost";
 var basePath = "/api";
+var debug = true;
 
 function url(path) {
     if (!path.startsWith("/"))
@@ -9,26 +10,41 @@ function url(path) {
     return domain + ":" + port + basePath + path;
 }
 
-var headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
+var corsHeaders = {
+'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+    'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT'
+}
+
+var myHeaders = {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
 };
 
-var getConf = { method: 'GET', header: headers, mode: "cors" };
-var postConf = { method: 'POST', header: headers, mode: "cors" };
-var putConf = { method: 'PUT', header: headers, mode: "cors" };
-var deleteConf = { method: 'DELETE', header: headers, mode: "cors" };
+var getConf = { method: 'GET', headers: myHeaders, mode: "cors" };
+var postConf = { method: 'POST', headers: myHeaders, mode: "cors" };
+var putConf = { method: 'PUT', headers: myHeaders, mode: "cors" };
+var deleteConf = { method: 'DELETE', headers: myHeaders, mode: "cors" };
 
 var tbody = document.getElementById("tbody");
+var personForm = {
+    id: document.getElementById("id"),
+    firstName: document.getElementById("firstName"),
+    lastName: document.getElementById("lastName"),
+    email: document.getElementById("email")
+}
+var personModal = document.getElementById("person-modal");
+var personModalTitle = document.getElementById("person-modal-title");
+var save = document.getElementById("save");
+var add = document.getElementById("add");
 var refresh = document.getElementById("refresh");
 
 var persons = [{
     id: 1,
-    email: "asd@asd.com",
     firstName: "First Name",
-    lastName: "Last Name"
+    lastName: "Last Name",
+    email: "asd@asd.com"
 }];
 
 function updateTable() {
@@ -36,26 +52,28 @@ function updateTable() {
         return "<tr><td>" +
             p.id +
             "</td><td>" +
-            p.email +
-            "</td><td>" +
             p.firstName +
             "</td><td>" +
             p.lastName +
             "</td><td>" +
-            "<button class= 'btn btn-warning edit' value=" + p.id + ">edit</button> " +
-            "<button class= 'btn btn-danger delete' value=" + p.id + ">delete</button>" +
+            p.email +
+            "</td><td>" +
+            "<div class='btn-group'>" +
+            "<button class= 'btn btn-warning btn-sm edit' value=" + p.id + ">edit</button> " +
+            "<button class= 'btn btn-danger btn-sm delete' value=" + p.id + ">delete</button>" +
+            "</div>" +
             "</td></tr>";
     }).join("\n");
     var del = document.getElementsByClassName("delete");
     var edit = document.getElementsByClassName("edit");
     for (var i = 0; i < del.length; i++) {
         del[i].addEventListener("click", delHandler);
-        //edit[i].addEventListener("click", editHandler);
+        edit[i].addEventListener("click", editHandler);
     }
 }
 
 function refreshHandler() {
-    console.log("refreshHandler on url: " + url("person"));
+    log("refreshHandler on url: " + url("person"));
 
     var status = false;
     var promise = fetch(url("person/contactinfo"), getConf);
@@ -74,6 +92,64 @@ function refreshHandler() {
 }
 refresh.addEventListener('click', refreshHandler);
 
+function addHandler() {
+    personModalTitle.innerText = "Add Person";
+    personModalTitle["x-handler"] = "Add Person";
+    $('#person-modal').modal('show');
+    $('#firstName').focus();
+}
+add.addEventListener('click', addHandler);
+
+function editHandler(evt) {
+    personModalTitle.innerText = "Edit Person";
+    personModalTitle["x-handler"] = "Edit Person";
+    var p = findPerson(evt.target.value);
+    personForm.id.value = p.id;
+    personForm.firstName.value = p.firstName;
+    personForm.lastName.value = p.lastName;
+    personForm.email.value = p.email;
+    $('#person-modal').modal('show');
+    $('#firstName').focus();
+}
+
+function saveHandler(evt) {
+    var p = {};
+    var conf = postConf;
+    if (personModalTitle["x-handler"] === "Edit Person") {
+        conf = putConf;
+        p.id = personForm.id.value;
+    } else if (personModalTitle["x-handler"] !== "Add Person") {
+        log("Fail: some how you tried to save the modal without it being opened correctly!");
+        return;
+    }
+    p.firstName = personForm.firstName.value;
+    p.lastName = personForm.lastName.value;
+    p.email = personForm.email.value;
+
+var json = JSON.stringify(p);
+    conf.body = json;
+    conf.headers["Content-Length"] = json.length;
+
+    var promise = fetch(url("person"), conf);
+    promise.then(function (res) {
+        log(res);
+        refreshHandler();
+        $('#person-modal').modal('hide');
+    }).catch(function () {
+        personModalTitle.innerText = personModalTitle["x-handler"] + ", failed to save";
+        log("Fail: couldn't save");
+    });
+}
+save.addEventListener('click', saveHandler);
+
+$("#person-modal").on('hidden.bs.modal', function (e) {
+    personModalTitle["x-handler"] = "";
+    personForm.id.value = "";
+    personForm.firstName.value = "";
+    personForm.lastName.value = "";
+    personForm.email.value = "";
+});
+
 function delHandler(evt) {
     var id = parseInt(evt.target.value);
     var delpromise = fetch(url("person") + "/" + id, deleteConf);
@@ -90,25 +166,19 @@ function delHandler(evt) {
 }
 
 function removePerson(id) {
-    var i = findIndex(id);
+    var i = 0;
+    for (; i < persons.length; i++) if (persons[i].id === id) break;
     var person = persons[i];
     persons.splice(i, 1);
     return person;
 }
 
-function editPerson(person) {
-    persons[findIndex(person.id)] = person;
-    return person;
-}
-
 function findPerson(id) {
-    return persons[findIndex(id)];
+    return persons.find(function (person) {
+        return person.id == id;
+    });
 }
 
-function findIndex(id) {
-    var i = 0;
-    for (; i < persons.length; i++)
-        if (persons[i].id === id) break;
-
-    return i;
+function log(message) {
+    if(debug) console.log(message);
 }
