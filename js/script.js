d318f9a1bbf74a2ae76fec5b71b4cd9dd2483a5f
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-03-17 10:17:17 +0100

    Made refresh work yay!

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ function url(path) {
         path = "/" + path;
     return domain + ":" + port + basePath + path;
 }
-;
 
 var headers = {
     'Access-Control-Allow-Origin': '*',
@@ -17,35 +16,35 @@ var headers = {
     'Accept': 'application/json'
 };
 
-var getConf = {method: 'GET', header: headers};
-var postConf = {method: 'POST', header: headers};
-var putConf = {method: 'PUT', header: headers};
-var deleteConf = {method: 'DELETE', header: headers};
+var getConf = { method: 'GET', header: headers, mode: "cors" };
+var postConf = { method: 'POST', header: headers, mode: "cors" };
+var putConf = { method: 'PUT', header: headers, mode: "cors" };
+var deleteConf = { method: 'DELETE', header: headers, mode: "cors" };
 
 var tbody = document.getElementById("tbody");
 var refresh = document.getElementById("refresh");
 
 var persons = [{
-        id: 1,
-        email: "asd@asd.com",
-        firstName: "First Name",
-        lastName: "Last Name"
-    }];
+    id: 1,
+    email: "asd@asd.com",
+    firstName: "First Name",
+    lastName: "Last Name"
+}];
 
 function updateTable() {
     tbody.innerHTML = persons.map(function (p) {
         return "<tr><td>" +
-                p.id +
-                "</td><td>" +
-                p.email +
-                "</td><td>" +
-                p.firstName +
-                "</td><td>" +
-                p.lastName +
-                "</td><td>" +
-                "<button class= 'btn btn-warning edit' value=" + p.id + ">edit</button> " +
-                "<button class= 'btn btn-danger delete' value=" + p.id + ">delete</button>" +
-                "</td></tr>";
+            p.id +
+            "</td><td>" +
+            p.email +
+            "</td><td>" +
+            p.firstName +
+            "</td><td>" +
+            p.lastName +
+            "</td><td>" +
+            "<button class= 'btn btn-warning edit' value=" + p.id + ">edit</button> " +
+            "<button class= 'btn btn-danger delete' value=" + p.id + ">delete</button>" +
+            "</td></tr>";
     }).join("\n");
     var del = document.getElementsByClassName("delete");
     var edit = document.getElementsByClassName("edit");
@@ -53,16 +52,35 @@ function updateTable() {
         del[i].addEventListener("click", delHandler);
         //edit[i].addEventListener("click", editHandler);
     }
-};
-refresh.addEventListener('click', updateTable);
+}
+
+function refreshHandler() {
+    console.log("refreshHandler on url: " + url("person"));
+
+    var promise = fetch(url("person"), getConf);
+
+    var status = false;
+    promise.then(function (response) {
+        status = response.ok;
+        return response.json();
+    }).then(function (json) {
+        if (status) {
+            persons = json;
+            updateTable();
+        } else {
+            // display err
+        }
+    });
+}
+refresh.addEventListener('click', refreshHandler);
 
 function delHandler(evt) {
     var delbody = parseInt(evt.target.value);
     var delpromise = fetch(url("person") + "/" + delbody, confDel);
     delpromise.then(function (response) {
-        return response.statusCode();
+        return response.ok;
     }).then(function (status) {
-        if (status != 200) {
+        if (status) {
             //Display error message and break.... alert("FEJL!!!");
         } else {
             for (var i = 0; i < persons.length; i++) {
@@ -74,4 +92,4 @@ function delHandler(evt) {
             updateTable();
         }
     });
-};
+}
