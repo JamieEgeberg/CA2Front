1009dfd4fed757576c3336199b9e024910934f52
JamieEgeberg <jamie_egeberg@hotmail.com>
2017-03-17 09:31:29 +0100

    DeleteHandler tiny fix

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ refresh.addEventListener('click', updateTable);
 
 function delHandler(evt) {
     var delbody = parseInt(evt.target.value);
-    var delpromise = fetch(url("person") + "/" + delbody, confDel);
+    var delpromise = fetch(url("person") + "/" + delbody, deleteConf);
     delpromise.then(function (response) {
         return response.statusCode();
     }).then(function (status) {
