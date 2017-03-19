ce27ff7bc25266fb12f0a1bb50083fb50968eec4
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-03-19 06:44:50 +0100

    Late night commit, almost forgot commiting before going to sleep

--- js/cityScript.js
+++ js/cityScript.js
@@ -31,71 +31,67 @@ var putConf = { method: 'PUT', headers: myHeaders, mode: "cors" };
 var deleteConf = { method: 'DELETE', headers: myHeaders, mode: "cors" };
 
 var tbody = document.getElementById("tbody"); // table body
-var modal = $("#person-modal"); // JQuery required for Bootstrap modals
-var modalTitle = document.getElementById("person-modal-title");
+var modal = $("#modal"); // JQuery required for Bootstrap modals
+var modalTitle = document.getElementById("modal-title");
 // simplifies inputfields by grouping them together
 var modalForm = {
     id: document.getElementById("id"),
-    firstName: document.getElementById("firstName"),
-    lastName: document.getElementById("lastName"),
-    email: document.getElementById("email")
+    zipCode: document.getElementById("zipCode"),
+    name: document.getElementById("name")
 };
 var save = document.getElementById("save"); // Save Changes button
 var add = document.getElementById("add"); // '+' button
 var refresh = document.getElementById("refresh"); // '↻' button
 
 // have a placeholding person which is overridden
-var persons = [{
+var cities = [{
     id: 0,
-    firstName: "placeholder",
-    lastName: "placeholder",
-    email: "placeholder@placeholder.com"
+    zipCode: "placeholder",
+    name: "placeholder"
 }];
 
 // Updates the tbody of table with rows
 function updateTable() {
-    tbody.innerHTML = persons.map(function (p) {
+    tbody.innerHTML = cities.map(function (p) {
         return "<tr><td>" +
             p.id +
             "</td><td>" +
-            p.firstName +
+            p.zipCode +
             "</td><td>" +
-            p.lastName +
+            p.name +
             "</td><td>" +
-            p.email +
-            "</td><td>" +
-            "<div class='btn-group'>" +
+            /*"<div class='btn-group'>" +
             "<button class= 'btn btn-warning btn-sm edit' value=" + p.id + ">edit</button> " +
             "<button class= 'btn btn-danger btn-sm delete' value=" + p.id + ">delete</button>" +
-            "</div>" +
+            "</div>" +*/
             "</td></tr>";
     }).join("\n");
-    var del = document.getElementsByClassName("delete");
-    var edit = document.getElementsByClassName("edit");
+    /*var del = document.getElementsByClassName("delete");
+    var edit = document.getElementsByClassName("edit");*/
     /* 
     this should be a safe for loop
     since the amount of delete and edit 
     buttons should be the same 
     */
-    for (var i = 0; i < del.length; i++) {
+    /*for (var i = 0; i < del.length; i++) {
         del[i].addEventListener("click", delHandler);
         edit[i].addEventListener("click", editHandler);
-    }
+    }*/
 }
 
 // '↻' button
 function refreshHandler() {
-    log("refreshHandler on url: " + url("person"));
+    log("refreshHandler on url: " + url("city"));
 
     var status = false;
-    var promise = fetch(url("person/contactinfo"), getConf);
+    var promise = fetch(url("city"), getConf);
 
     promise.then(function (response) {
         status = response.ok;
         return response.json();
     }).then(function (json) {
         if (status) {
-            persons = json;
+            cities = json;
             updateTable();
         } else {
             // display err
@@ -106,50 +102,46 @@ refresh.addEventListener('click', refreshHandler);
 
 // '+' button
 function addHandler() {
-    modalTitle.innerText = "Add Person";
-    modalTitle["x-handler"] = "Add Person";
-    $('#person-modal').modal('show');
-    $('#firstName').focus();
+    modalTitle.innerText = "Add City";
+    modalTitle["x-handler"] = "Add City";
+    $('#modal').modal('show');
 }
-add.addEventListener('click', addHandler);
+/*add.addEventListener('click', addHandler);*/
 
 // Edit buttons
 function editHandler(evt) {
-    modalTitle.innerText = "Edit Person";
-    modalTitle["x-handler"] = "Edit Person";
+    modalTitle.innerText = "Edit City";
+    modalTitle["x-handler"] = "Edit City";
     var p = find(evt.target.value);
     modalForm.id.value = p.id;
-    modalForm.zipCode.value = p.firstName;
-    modalForm.lastName.value = p.lastName;
-    modalForm.email.value = p.email;
-    $('#person-modal').modal('show');
-    $('#firstName').focus();
+    modalForm.zipCode.value = p.zipCode;
+    modalForm.name.value = p.name;
+    $('#modal').modal('show');
 }
 
 // When modal is saved.
 function saveHandler(evt) {
     var p = {};
     var conf = postConf;
-    if (modalTitle["x-handler"] === "Edit Person") {
+    if (modalTitle["x-handler"] === "Edit City") {
         conf = putConf;
         p.id = modalForm.id.value;
-    } else if (modalTitle["x-handler"] !== "Add Person") {
+    } else if (modalTitle["x-handler"] !== "Add City") {
         log("Fail: some how you tried to save the modal without it being opened correctly!");
         return;
     }
-    p.firstName = modalForm.zipCode.value;
-    p.lastName = modalForm.lastName.value;
-    p.email = modalForm.email.value;
+    p.zipCode = modalForm.zipCode.value;
+    p.name = modalForm.name.value;
 
     var json = JSON.stringify(p);
     conf.body = json;
     conf.headers["Content-Length"] = json.length;
 
-    var promise = fetch(url("person"), conf);
+    var promise = fetch(url("city"), conf);
     promise.then(function (res) {
         log(res);
         refreshHandler();
-        $('#person-modal').modal('hide');
+        $('#modal').modal('hide');
     }).catch(function () {
         modalTitle.innerText = modalTitle["x-handler"] + ", failed to save";
         log("Fail: couldn't save");
@@ -158,18 +150,17 @@ function saveHandler(evt) {
 save.addEventListener('click', saveHandler);
 
 // Bootstrap related, this is done when the modal is closed.
-$("#person-modal").on('hidden.bs.modal', function (e) {
+$("#modal").on('hidden.bs.modal', function (e) {
     modalTitle["x-handler"] = "";
     modalForm.id.value = "";
     modalForm.zipCode.value = "";
-    modalForm.lastName.value = "";
-    modalForm.email.value = "";
+    modalForm.name.value = "";
 });
 
 // Delete Buttons
 function delHandler(evt) {
     var id = parseInt(evt.target.value);
-    var delpromise = fetch(url("person") + "/" + id, deleteConf);
+    var delpromise = fetch(url("city") + "/" + id, deleteConf);
     delpromise.then(function (response) {
         return response.ok;
     }).then(function (status) {
@@ -185,16 +176,16 @@ function delHandler(evt) {
 // Remove person in persons
 function remove(id) {
     var i = 0;
-    for (; i < persons.length; i++) if (persons[i].id === id) break;
-    var person = persons[i];
-    persons.splice(i, 1);
-    return person;
+    for (; i < cities.length; i++) if (cities[i].id === id) break;
+    var obj = cities[i];
+    cities.splice(i, 1);
+    return obj;
 }
 
 // Find person by id in persons
 function find(id) {
-    return persons.find(function (person) {
-        return person.id == id;
+    return cities.find(function (obj) {
+        return obj.id == id;
     });
 }
 
